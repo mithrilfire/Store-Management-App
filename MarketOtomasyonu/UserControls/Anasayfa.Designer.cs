ea93f36e0184ab4535447dc0f2872646609f2dba
Fatih Çalışır <27806840+mithrilfire@users.noreply.github.com>
2023-01-04 22:45:34 +0300

    anasayfaya buton eklendi

--- MarketOtomasyonu/UserControls/Anasayfa.Designer.cs
+++ MarketOtomasyonu/UserControls/Anasayfa.Designer.cs
@@ -28,12 +28,24 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(507, 641);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // Anasayfa
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.button1);
             this.Name = "Anasayfa";
             this.Size = new System.Drawing.Size(1171, 736);
             this.ResumeLayout(false);
@@ -41,5 +53,7 @@
         }
 
         #endregion
+
+        private Button button1;
     }
 }
