bbbb5b19b50ddeb5214b09846106989f2e218df7
AlperenTellioglu <alperen.tellioglu@bil.omu.edu.tr>
2023-01-05 18:42:33 +0300

    Stok user controlü oluşturuldu.

--- MarketOtomasyonu/UserControls/Stoklar.Designer.cs
+++ MarketOtomasyonu/UserControls/Stoklar.Designer.cs
@@ -28,18 +28,128 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.uKodu = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.irsaliyeNo = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.tNo = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.uBGFiyati = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.uAdeti = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(953, 172);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(82, 24);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "Listele";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(953, 106);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(117, 23);
+            this.textBox1.TabIndex = 8;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.label2.Location = new System.Drawing.Point(859, 109);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(64, 15);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Ürün Kodu";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.label1.Location = new System.Drawing.Point(966, 56);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(51, 15);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Stok Ara";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.uKodu,
+            this.irsaliyeNo,
+            this.tNo,
+            this.uBGFiyati,
+            this.uAdeti});
+            this.dataGridView1.Location = new System.Drawing.Point(24, 24);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 25;
+            this.dataGridView1.Size = new System.Drawing.Size(793, 684);
+            this.dataGridView1.TabIndex = 5;
+            // 
+            // uKodu
+            // 
+            this.uKodu.HeaderText = "Ürün Kodu";
+            this.uKodu.Name = "uKodu";
+            this.uKodu.Width = 150;
+            // 
+            // irsaliyeNo
+            // 
+            this.irsaliyeNo.HeaderText = "İrsaliye No";
+            this.irsaliyeNo.Name = "irsaliyeNo";
+            this.irsaliyeNo.Width = 150;
+            // 
+            // tNo
+            // 
+            this.tNo.HeaderText = "Tedarikçi No";
+            this.tNo.Name = "tNo";
+            this.tNo.Width = 150;
+            // 
+            // uBGFiyati
+            // 
+            this.uBGFiyati.HeaderText = "Ürün Girdi Birim Fiyatı";
+            this.uBGFiyati.Name = "uBGFiyati";
+            this.uBGFiyati.Width = 150;
+            // 
+            // uAdeti
+            // 
+            this.uAdeti.HeaderText = "Ürün Adeti";
+            this.uAdeti.Name = "uAdeti";
+            this.uAdeti.Width = 150;
             // 
             // Stoklar
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridView1);
             this.Name = "Stoklar";
             this.Size = new System.Drawing.Size(1171, 736);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private Button button1;
+        private TextBox textBox1;
+        private Label label2;
+        private Label label1;
+        private DataGridView dataGridView1;
+        private DataGridViewTextBoxColumn uKodu;
+        private DataGridViewTextBoxColumn irsaliyeNo;
+        private DataGridViewTextBoxColumn tNo;
+        private DataGridViewTextBoxColumn uBGFiyati;
+        private DataGridViewTextBoxColumn uAdeti;
     }
 }
