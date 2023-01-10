988842a37569d69ac9c8dd37032cbd39d6720289
sametdrmz <56392665+sametdrmz@users.noreply.github.com>
2023-01-10 22:50:28 +0300

    Musteriler ve Tedarikçi menuleri yapıldı. Ara, ekle, düzenle, kaldır ve temizle butonları çalışır haldedir.

--- MarketOtomasyonu/UserControls/Musteriler.cs
+++ MarketOtomasyonu/UserControls/Musteriler.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MarketOtomasyonu.Models;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -15,6 +16,124 @@ namespace MarketOtomasyonu.UserControls
         public Musteriler()
         {
             InitializeComponent();
+            GetFromDB();
+        }
+
+        void GetFromDB()
+        {
+            using (var db = new MarketDBContext())
+            {
+                var musteri = db.musteriler.ToList();
+                BindingSource src = new BindingSource();
+                src.DataSource = musteri;
+                musteridatagrid.DataSource = src;
+            }
+        }
+        int musid;
+        private void musteridatagrid_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            int id = (int)musteridatagrid[0, e.RowIndex].Value;
+
+            using (var db = new MarketDBContext())
+            {
+                Models.Musteri musteri = db.musteriler.
+                    Where(m => m.MusteriId == id).
+                    First();
+                musAdiTxtbox.Text = musteri.Adi;
+                musSoyadiTxtbox.Text = musteri.Soyadi;
+                musid = musteri.MusteriId;
+            }
+        }
+
+        private void ekleBtn_Click(object sender, EventArgs e)
+        {
+            using (var db = new MarketDBContext())
+            {
+                Models.Musteri musteri = new Models.Musteri();
+
+                musteri.Adi = musAdiTxtbox.Text;
+                musteri.Soyadi = musSoyadiTxtbox.Text;
+                db.musteriler.Add(musteri);
+                db.SaveChanges();
+            }
+            GetFromDB();
+        }
+
+        private void duzenleBtn_Click(object sender, EventArgs e)
+        {
+            using (var db = new MarketDBContext())
+            {
+                var musteriler = db.musteriler.Where(m => m.MusteriId == musid);
+
+                if (musteriler.Any())
+                {
+                    Models.Musteri musteri = musteriler.First();
+
+                    musteri.Adi = musAdiTxtbox.Text;
+                    musteri.Soyadi = musSoyadiTxtbox.Text;
+                }
+
+                db.SaveChanges();
+            }
+
+            GetFromDB();
+        }
+
+        private void araBtn_Click(object sender, EventArgs e)
+        {
+            using (var db = new MarketDBContext())
+            {
+                if (musAdiTxtbox.Text != "" && musSoyadiTxtbox.Text != "")
+                {
+                    var musteriad = db.musteriler.Where(m => m.Adi == musAdiTxtbox.Text && m.Soyadi == musSoyadiTxtbox.Text);
+                    musteridatagrid.DataSource = musteriad.ToList();
+                }
+                else if (musAdiTxtbox.Text != "" && musSoyadiTxtbox.Text == "")
+                {
+                    var musteriad = db.musteriler.Where(m => m.Adi == musAdiTxtbox.Text);
+                    musteridatagrid.DataSource = musteriad.ToList();
+                }
+                else if (musAdiTxtbox.Text == "" && musSoyadiTxtbox.Text != "")
+                {
+                    var musterisoyad = db.musteriler.Where(m => m.Soyadi == musSoyadiTxtbox.Text);
+                    musteridatagrid.DataSource = musterisoyad.ToList();
+                }
+                else
+                {
+
+                    GetFromDB();
+                }
+            }
+        }
+
+        private void kaldirBtn_Click(object sender, EventArgs e)
+        {
+            using (var db = new MarketDBContext())
+            {
+                var musteriler = db.musteriler.Where(m => m.MusteriId == musid);
+
+                if (musteriler.Any())
+                {
+                    db.musteriler.Remove(musteriler.First());
+                }
+                else
+                {
+                    uyariLbl.Text = "Bu Id ile eşleşen herhangi bir kullanıcı bulunamadı.";
+                    uyariLbl.Visible = true;
+                    return;
+                }
+
+                db.SaveChanges();
+            }
+
+            GetFromDB();
+        }
+
+        private void temizleBtn_Click(object sender, EventArgs e)
+        {
+            GetFromDB();
+            musAdiTxtbox.Clear();
+            musSoyadiTxtbox.Clear();
         }
     }
 }
--- MarketOtomasyonu/UserControls/Tedarikci.cs
+++ MarketOtomasyonu/UserControls/Tedarikci.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MarketOtomasyonu.Models;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -15,6 +16,94 @@ namespace MarketOtomasyonu.UserControls
         public Tedarikci()
         {
             InitializeComponent();
+            GetFromDB();
+        }
+
+        void GetFromDB()
+        {
+            using (var db = new MarketDBContext())
+            {
+                var tedarikci = db.tedarikciler.ToList();
+                BindingSource src = new BindingSource();
+                src.DataSource = tedarikci;
+                tedarikciDataGrid.DataSource = src;
+            }
+        }
+
+        int tedid;
+
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            int id = (int)tedarikciDataGrid[0, e.RowIndex].Value;
+
+            using (var db = new MarketDBContext())
+            {
+                Models.Tedarikci tedarikci = db.tedarikciler.
+                    Where(t => t.TedarikciId == id).
+                    First();
+                tedarikciTxtbox.Text = tedarikci.Adi;
+                tedarikçiAdresTxtbox.Text = tedarikci.Adres;
+                tedid = tedarikci.TedarikciId;
+            }
+        }
+
+        private void ekleBtn_Click(object sender, EventArgs e)
+        {
+            using (var db = new MarketDBContext())
+            {
+                Models.Tedarikci tedarikci = new Models.Tedarikci();
+
+                tedarikci.Adi = tedarikciTxtbox.Text;
+                tedarikci.Adres = tedarikçiAdresTxtbox.Text;
+                db.tedarikciler.Add(tedarikci);
+                db.SaveChanges();
+            }
+            GetFromDB();
+        }
+
+        private void araBtn_Click(object sender, EventArgs e)
+        {
+            using (var db = new MarketDBContext())
+            {
+                var tedarikci = db.tedarikciler.Where(t => t.Adi == araTxtbox.Text);
+                tedarikciDataGrid.DataSource = tedarikci.ToList();
+                if (araTxtbox.Text == "" )
+                {
+                    GetFromDB();
+                }
+                
+            }
+            
+
+        }
+
+        private void duzenleBtn_Click(object sender, EventArgs e)
+        {
+
+            using (var db = new MarketDBContext())
+            {
+                var tedarikciler = db.tedarikciler.Where(t => t.TedarikciId == tedid);
+
+                if (tedarikciler.Any())
+                {
+                    Models.Tedarikci tedarikci = tedarikciler.First();
+
+                    tedarikci.Adi = tedarikciTxtbox.Text;
+                    tedarikci.Adres = tedarikçiAdresTxtbox.Text;
+                }
+
+                db.SaveChanges();
+            }
+
+            GetFromDB();
+        }
+
+        private void temizleBtn_Click(object sender, EventArgs e)
+        {
+            GetFromDB();
+            araTxtbox.Clear();
+            tedarikciTxtbox.Clear();
+            tedarikçiAdresTxtbox.Clear();
         }
     }
 }
