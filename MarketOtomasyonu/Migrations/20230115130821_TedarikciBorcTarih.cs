a08cf9f2f6eaf8bf9884a32c76c80165edf38564
Fatih Çalışır <27806840+mithrilfire@users.noreply.github.com>
2023-01-15 16:47:49 +0300

    tedarikci borc modeline tarih eklendi.

---
+++ MarketOtomasyonu/Migrations/20230115130821_TedarikciBorcTarih.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MarketOtomasyonu.Migrations
+{
+    /// <inheritdoc />
+    public partial class TedarikciBorcTarih : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "Tarih",
+                table: "tedarikciBorclar",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Tarih",
+                table: "tedarikciBorclar");
+        }
+    }
+}
